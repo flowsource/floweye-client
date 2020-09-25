16b3535931367c7968c5abd51f009a9d575dedef
vody105 <filipsuska@gmail.com>
2020-09-25 14:19:29 +0200

    Update /user-groups

--- src/Entity/UserGroupEditEntity.php
+++ src/Entity/UserGroupEditEntity.php
@@ -5,35 +5,27 @@ namespace Floweye\Client\Entity;
 class UserGroupEditEntity
 {
 
-	/** @var int */
-	private $id;
-
-	/** @var string */
-	private $gid;
-
 	/** @var string */
 	private $name;
 
-	public function __construct(int $id, string $gid, string $name)
+	public function __construct(string $name)
 	{
-		$this->id = $id;
-		$this->gid = $gid;
 		$this->name = $name;
-	}
-
-	public function getId(): int
-	{
-		return $this->id;
-	}
-
-	public function getGid(): string
-	{
-		return $this->gid;
 	}
 
 	public function getName(): string
 	{
 		return $this->name;
+	}
+
+	/**
+	 * @return mixed[]
+	 */
+	public function toBody(): array
+	{
+		return [
+			'name' => $this->name,
+		];
 	}
 
 }
