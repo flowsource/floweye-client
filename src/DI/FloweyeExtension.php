dd73fa3d87137dc747c022cd15c7469d5fcca70d
Filip Šuška <filipsuska@gmail.com>
2024-01-30 14:49:13 +0100

    Drop removed endpoints

--- src/DI/FloweyeExtension.php
+++ src/DI/FloweyeExtension.php
@@ -3,19 +3,11 @@
 namespace Floweye\Client\DI;
 
 use Floweye\Client\Client\ApplicationClient;
-use Floweye\Client\Client\PlanClient;
-use Floweye\Client\Client\ProcessClient;
-use Floweye\Client\Client\TemplateProcessClient;
-use Floweye\Client\Client\TimerClient;
 use Floweye\Client\Client\UserClient;
 use Floweye\Client\Client\UserGroupClient;
 use Floweye\Client\Http\Guzzle\GuzzleFactory;
 use Floweye\Client\Http\HttpClient;
 use Floweye\Client\Service\ApplicationService;
-use Floweye\Client\Service\PlanService;
-use Floweye\Client\Service\ProcessService;
-use Floweye\Client\Service\TemplateProcessService;
-use Floweye\Client\Service\TimerService;
 use Floweye\Client\Service\UserGroupService;
 use Floweye\Client\Service\UserService;
 use Nette\DI\CompilerExtension;
@@ -63,45 +55,21 @@ class FloweyeExtension extends CompilerExtension
 		$builder->addDefinition($this->prefix('client.application'))
 			->setFactory(ApplicationClient::class, [$this->prefix('@http.client')]);
 
-		$builder->addDefinition($this->prefix('client.templateProcess'))
-			->setFactory(TemplateProcessClient::class, [$this->prefix('@http.client')]);
-
-		$builder->addDefinition($this->prefix('client.plan'))
-			->setFactory(PlanClient::class, [$this->prefix('@http.client')]);
-
-		$builder->addDefinition($this->prefix('client.process'))
-			->setFactory(ProcessClient::class, [$this->prefix('@http.client')]);
-
 		$builder->addDefinition($this->prefix('client.user'))
 			->setFactory(UserClient::class, [$this->prefix('@http.client')]);
 
 		$builder->addDefinition($this->prefix('client.userGroup'))
 			->setFactory(UserGroupClient::class, [$this->prefix('@http.client')]);
 
-		$builder->addDefinition($this->prefix('client.timer'))
-			->setFactory(TimerClient::class, [$this->prefix('@http.client')]);
-
 		// #3 Services
 		$builder->addDefinition($this->prefix('service.application'))
 			->setFactory(ApplicationService::class, [$this->prefix('@client.application')]);
-
-		$builder->addDefinition($this->prefix('service.templateProcess'))
-			->setFactory(TemplateProcessService::class, [$this->prefix('@client.templateProcess')]);
-
-		$builder->addDefinition($this->prefix('service.plan'))
-			->setFactory(PlanService::class, [$this->prefix('@client.plan')]);
-
-		$builder->addDefinition($this->prefix('service.process'))
-			->setFactory(ProcessService::class, [$this->prefix('@client.process')]);
 
 		$builder->addDefinition($this->prefix('service.user'))
 			->setFactory(UserService::class, [$this->prefix('@client.user')]);
 
 		$builder->addDefinition($this->prefix('service.userGroup'))
 			->setFactory(UserGroupService::class, [$this->prefix('@client.userGroup')]);
-
-		$builder->addDefinition($this->prefix('service.timer'))
-			->setFactory(TimerService::class, [$this->prefix('@client.timer')]);
 	}
 
 }
--- tests/Unit/DI/FloweyeExtensionTest.php
+++ tests/Unit/DI/FloweyeExtensionTest.php
@@ -3,10 +3,6 @@
 namespace Tests\Floweye\Client\Unit\DI;
 
 use Floweye\Client\Client\ApplicationClient;
-use Floweye\Client\Client\PlanClient;
-use Floweye\Client\Client\ProcessClient;
-use Floweye\Client\Client\TemplateProcessClient;
-use Floweye\Client\Client\TimerClient;
 use Floweye\Client\Client\UserClient;
 use Floweye\Client\Client\UserGroupClient;
 use Floweye\Client\DI\FloweyeExtension;
@@ -14,10 +10,6 @@ use Floweye\Client\DI\FloweyeExtension24;
 use Floweye\Client\Http\Guzzle\GuzzleFactory;
 use Floweye\Client\Http\HttpClient;
 use Floweye\Client\Service\ApplicationService;
-use Floweye\Client\Service\PlanService;
-use Floweye\Client\Service\ProcessService;
-use Floweye\Client\Service\TemplateProcessService;
-use Floweye\Client\Service\TimerService;
 use Floweye\Client\Service\UserGroupService;
 use Floweye\Client\Service\UserService;
 use Nette\DI\Compiler;
@@ -58,20 +50,12 @@ class FloweyeExtensionTest extends ContainerTestCase
 		static::assertInstanceOf(HttpClient::class, $container->getService('ispa.apis.http.client'));
 
 		static::assertInstanceOf(ApplicationClient::class, $container->getService('ispa.apis.client.application'));
-		static::assertInstanceOf(TemplateProcessClient::class, $container->getService('ispa.apis.client.templateProcess'));
-		static::assertInstanceOf(PlanClient::class, $container->getService('ispa.apis.client.plan'));
-		static::assertInstanceOf(ProcessClient::class, $container->getService('ispa.apis.client.process'));
 		static::assertInstanceOf(UserClient::class, $container->getService('ispa.apis.client.user'));
 		static::assertInstanceOf(UserGroupClient::class, $container->getService('ispa.apis.client.userGroup'));
-		static::assertInstanceOf(TimerClient::class, $container->getService('ispa.apis.client.timer'));
 
 		static::assertInstanceOf(ApplicationService::class, $container->getService('ispa.apis.service.application'));
-		static::assertInstanceOf(TemplateProcessService::class, $container->getService('ispa.apis.service.templateProcess'));
-		static::assertInstanceOf(PlanService::class, $container->getService('ispa.apis.service.plan'));
-		static::assertInstanceOf(ProcessService::class, $container->getService('ispa.apis.service.process'));
 		static::assertInstanceOf(UserService::class, $container->getService('ispa.apis.service.user'));
 		static::assertInstanceOf(UserGroupService::class, $container->getService('ispa.apis.service.userGroup'));
-		static::assertInstanceOf(TimerService::class, $container->getService('ispa.apis.service.timer'));
 	}
 
 }
